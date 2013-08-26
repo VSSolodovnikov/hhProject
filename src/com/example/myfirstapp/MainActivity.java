da46c0260506c19e64fb562d0524d7f3434c3993
VSSolodovnikov <vs.solodovnikov@gmail.com>
2013-08-27 00:09:29 +0400

    first commit

---
+++ src/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.myfirstapp;
+
+import java.util.Calendar;
+
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.support.v4.app.DialogFragment;
+
+@SuppressLint({ "ValidFragment", "NewApi" })
+public class MainActivity extends FragmentActivity {
+
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	
+	EditText mEdit;
+
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.sex_array, android.R.layout.simple_spinner_item);
+    	adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    	Spinner spinner = (Spinner) findViewById(R.id.sex_spinner);
+    	spinner.setAdapter(adapter);
+    	spinner.setPrompt("Пол");
+    	spinner.setSelection(0);
+        
+    	
+    }
+    
+	public void selectDate(View view) {
+		DialogFragment newFragment = new SelectDateFragment();
+		newFragment.show(getSupportFragmentManager(), "DatePicker");
+	}
+	
+	public void populateSetDate(int year, int month, int day) {
+		mEdit = (EditText)findViewById(R.id.bornDate);
+		mEdit.setText(month+"/"+day+"/"+year);
+	}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    /** Called when the user clicks the Send button */
+    @SuppressLint("NewApi")
+	public void sendMessage(View view) {
+       
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
+	
+    }
+    
+    public class SelectDateFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+    	@SuppressLint("NewApi")
+		@Override
+    	public Dialog onCreateDialog(Bundle savedInstanceState) {
+    	final Calendar calendar = Calendar.getInstance();
+    	int yy = calendar.get(Calendar.YEAR);
+    	int mm = calendar.get(Calendar.MONTH);
+    	int dd = calendar.get(Calendar.DAY_OF_MONTH);
+    	return new DatePickerDialog(getActivity(), this, yy, mm, dd);
+    	}
+    	 
+    	public void onDateSet(DatePicker view, int yy, int mm, int dd) {
+    	populateSetDate(yy, mm+1, dd);
+    	}
+    }
+    
+}
